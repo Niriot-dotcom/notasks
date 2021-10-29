63c7a7ad0a2d8e801a7ba86bf2c74aad4ddf927f
Niriot <lopezpatyyarely@gmail.com>
2021-10-29 02:46:27 -0500

    React implementation & file structure

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NoteCard from "./components/notes/NoteCard";
+
+function App() {
+  return (
+    <div>
+      <NoteCard />
+      <center>React here!</center>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/notes/NoteCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NoteCard() {
+    return (
+        <nav className="navbar bg-dark container">
+            <h4>Home</h4>
+            <h4>Mexico</h4>
+            <h4>Ecuador</h4>
+        </nav>
+    );
+}
+
+export default NoteCard;
