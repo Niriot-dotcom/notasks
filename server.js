01c53a1642fd11583866382445eb59d14e34db28
Ulises <89113180+UlisesGallardo@users.noreply.github.com>
2021-11-26 17:22:49 -0600

    Actualizando Server en el Pull request del heatmap

--- server.js
+++ server.js
@@ -26,7 +26,9 @@ MongoClient.connect(connectionString, (err, client) => {
     const db = client.db('NoTasks')                             //Conectando a base de datos Notasks
     const tasks = db.collection('notas')                        //Creando coleccion
     const users = db.collection("Usuarios")                     //Para manejo de cuentas
-    
+    var ObjectID = require('mongodb').ObjectID;
+
+
     // Routes
     app.get('/api', (req, res)=>{
         res.send(`
@@ -46,7 +48,6 @@ MongoClient.connect(connectionString, (err, client) => {
     })
 
     app.get('/api/notes/:id', (req, res)=>{
-        var ObjectID = require('mongodb').ObjectID;
         const id = new ObjectID(req.params.id);
         db.collection('notas').find({ id_usuario: id }).toArray() 
         .then((resultado) => {
@@ -71,7 +72,6 @@ MongoClient.connect(connectionString, (err, client) => {
     
     app.delete("/api/notes/delete/:idNote", (req, res) => {
         console.log("req.idNote params: ", req.params.idNote);
-        var ObjectId = require('mongodb').ObjectId;
         let idNote = new ObjectId(req.params.idNote);
         db.collection('notas').deleteOne(
             // { _id: req.params.idNote }
@@ -89,32 +89,51 @@ MongoClient.connect(connectionString, (err, client) => {
         })
     })
 
-    app.post('/api/user/register', (req, res) => {
-        console.log("user", req.body.user)
+    let validar = p => {
+        var n = false, l = false, u = false, lg = false, s = false;
+        for(let i = 0; i < p.password.length; i++){
+            var ch = p.password[i];
+            if(isNaN(ch)){
+                if(ch.toLowerCase() == ch.toUpperCase())
+                    s = true;
+                else if(ch == ch.toLowerCase())
+                    l = true;
+                else u = true;
+            }
+            else n = true;
+        }
+        lg = p.password.length >= 8;
+        return lg && n && l && u && s;
+    };
 
-        console.log("The mail", req.body.mail)
-        console.log("Password", req.body.password)
-        console.log("University", req.body.university)
+
+    app.post('/api/user/register', (req, res) => {
         const persona = {
             user: req.body.user,
             mail: req.body.mail,
             password: req.body.password,
             university: req.body.university
         }
-        users.insertOne(persona)
-        .then(resultado => {
-            console.log("Nuevo usuario creado", resultado);
-            res.send({
-                id: resultado.insertedId,
-                isLogged: true
+        var eU = false, eM = false, vP = false, vU = false;
+        db.collection('Usuarios').find(persona.user).toArray()
+        .then(resultado => eU = resultado.length);
+        db.collection('Usuarios').find(persona.mail).toArray()
+        .then(resultado => eM = resultado.length);
+        vP = validar(persona);
+        if(!eU && !eM && vP){ 
+            users.insertOne(persona)
+            .then(resultado => {
+                console.log("Nuevo usuario creado", resultado);
+                res.send({
+                    id: resultado.insertedId,
+                    isLogged: true
+                })
             })
-        })
-        .catch((error) => console.error(error))
+            .catch((error) => console.error(error))
+        }
     })
 
     app.post('/api/user/login', (req, res) => {
-        console.log("user", req.body.user)
-        console.log("Password", req.body.password)
         const persona = {
             "user": req.body.user,
             "password": req.body.password
@@ -135,44 +154,7 @@ MongoClient.connect(connectionString, (err, client) => {
     })
 
     app.post('/api/actualizar/:id', (req, res) => {
-        console.log("Body request title: ", req.body.correo)
-        console.log("Body request body: ", req.body.contrasena)
-        console.log("Body request body: ", req.body.universidad)
-        console.log("Body request body: ", req.body.usuario)
-
-        var ObjectID = require('mongodb').ObjectID;
         const { id } = req.params;
-
-        if(req.body.usuario != ""){
-            db.collection('Usuarios').findOneAndUpdate(
-                {"_id": ObjectID(id)},
-                {
-                    $set:{
-                        user:req.body.usuario,
-                    }
-                }
-            )
-            .then(result => {
-                console.log("Modificado correctamente: ",result)
-                })
-            .catch(error => console.error(error))
-        }
-
-        if(req.body.correo != ""){
-            db.collection('Usuarios').findOneAndUpdate(
-                {"_id": ObjectID(id)},
-                {
-                    $set:{
-                        mail:req.body.correo
-                    }
-                }
-            )
-            .then(result => {
-                console.log("Modificado correctamente: ",result)
-                })
-            .catch(error => console.error(error))
-        }
-
         if(req.body.contrasena != ""){
             db.collection('Usuarios').findOneAndUpdate(
                 {"_id": ObjectID(id)},
@@ -187,29 +169,11 @@ MongoClient.connect(connectionString, (err, client) => {
                 })
             .catch(error => console.error(error))
         }
-
-        if(req.body.universidad != ""){
-            db.collection('Usuarios').findOneAndUpdate(
-                {"_id": ObjectID(id)},
-                {
-                    $set:{
-                        university:req.body.universidad
-                    }
-                }
-            )
-            .then(result => {
-                console.log("Modificado correctamente: ",result)
-                })
-            .catch(error => console.error(error))
-            }
-        
             res.sendStatus(200);
     })
 
     app.delete('/api/eliminarcuenta/:id', (req,res)=>{
-        var ObjectID = require('mongodb').ObjectID;
         const { id } = req.params;
-        
         db.collection('Usuarios').deleteOne(
             { _id: ObjectID(id) }
         )
@@ -218,13 +182,10 @@ MongoClient.connect(connectionString, (err, client) => {
             console.log(resultado);
         })
         .catch(error => console.error(error))
-        
     })
 
     app.get('/api/usuario/:id',(req, res)=>{
-        var ObjectID = require('mongodb').ObjectID;
         const { id } = req.params;
-
         db.collection('Usuarios').find( { _id: ObjectID(id) } ).toArray()
         .then((resultado) => {
             res.send(resultado)
